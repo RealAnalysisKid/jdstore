f8e30d806807f80b68c8342f1d4f66959de1d2a2
RealAnalysis <detective_boys@hotmail.com>
2017-02-27 11:45:13 +0800

    install fog properly

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,19 @@
 CarrierWave.configure do |config|
-  config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["Sk-seimaTqUuB8iKOj2LWPEbBMODXpkrQgKH7oGy"]
-  config.qiniu_secret_key    = ENV["0dZ_O2yte6iamd3SzOsVcI0MnWR1k4bXoA-ZQ0yu"]
-  config.qiniu_bucket        = ENV["realanalysis"]
-  config.qiniu_bucket_domain = ENV["olzw7m80w.bkt.clouddn.com"]
-  config.qiniu_block_size    = 4*1024*1024
-  config.qiniu_protocol      = "http"
-  config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
+  if Rails.env.production?
+    config.fog_provider = 'fog'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+
+      region:                ENV["AWS_REGION"]
+
+    }
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
+
+
+  else
+    config.storage :file
+  end
 end
