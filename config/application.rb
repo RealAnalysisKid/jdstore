d0d47d3bfca3d949f89d50ea7b121c486dd6d45e
RealAnalysis <detective_boys@hotmail.com>
2017-03-01 22:34:32 +0800

    fix static assets

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# 解决本地跟heroku显示不同问题
+config.serve_static_assets = true
+
+
 module Jdstore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "jdstore_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # 解决本地跟heroku显示不同问题
+  config.serve_static_assets = true
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
